abca6f855074b51dbcd097d4bf4a6297b3e2653b
renfougouzai <1296087473@qq.com>
2026-04-07 10:12:13 +0800

    更新体系: 2026-04-07 10:12:13

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({ title: "目录" }),
   ],
   right: [
     Component.Graph(),
@@ -67,7 +67,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({ title: "目录" }),
   ],
   right: [],
 }
